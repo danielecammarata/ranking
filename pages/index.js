a6599334785b04df2bd7156ceb7eda60ae493ff0
Daniele Valentino Cammarata <DanieleValentino.Cammarata@vimn.com>
2018-07-25 18:10:22 +0200

    mongo first data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import Head from 'next/head'
+import Grid from '@material-ui/core/Grid'
 import Layout from '../components/Layout.js'
 
+import {
+  styleH1,
+} from '../lib/SharedStyles'
+
 export default () => (
-    <Layout>
-       <p>Hello Next.js</p>
-    </Layout>
+  <Layout>
+    <Head>
+      <title>Open source (MIT License) web app to publish documentation and books</title>
+      <meta
+        name="description"
+        content="Open source web app built with modern JavaScript stack: React, Material UI, Next, Express, Mongoose, and MongoDB. Integrated with AWS SES, Github, Google OAuth, Stripe, and MailChimp."
+      />
+    </Head>
+    <div style={{ padding: '10px 8%', fontSize: '15px' }}>
+      <Grid container direction="row" justify="space-around" align="flex-start">
+        <Grid item sm={12} xs={12} style={{ textAlign: 'center' }}>
+          <br />
+          <h1 style={styleH1}>RunKing</h1>
+        </Grid>
+      </Grid>
+    </div>
+  </Layout>
 )
--- server/add.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-router.route('/add/player').get((req, res) => res.send('add player'))
-router.route('/add/match').get((req, res) => res.send('add match'))
-
-module.exports = router
