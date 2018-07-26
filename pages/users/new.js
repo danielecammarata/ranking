99d334e8c35a3cafc186020d0c742e025f50ed22
Daniele Valentino Cammarata <DanieleValentino.Cammarata@vimn.com>
2018-07-26 14:17:47 +0200

    new user step 1

--- pages/users/new.js
+++ pages/users/new.js
@@ -1,33 +1,31 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
+import Divider from '@material-ui/core/Divider'
+import Button from '@material-ui/core/Button'
 
 import Layout from '../../components/Layout.js'
-import { addNewUser, getUsersList } from '../../lib/api/users'
-
+import { addNewUser } from '../../lib/api/users'
+import { styleH1, styleForm, styleTextField, styleRaisedButton } from '../../lib/SharedStyles'
 
 class AddUser extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      users: props.users || [],
-      userAdded: false
-    }
-  }
-
-  async componentDidMount() {
-    try {
-      const data = await getUsersList()
-      this.setState({ users: data.users })
-    } catch (err) {
-      console.log(err)
+      userAdded: false,
+      name: '',
+      avatarUrl: ''
     }
   }
 
   onSubmit = async (event) => {
     event.preventDefault()
-    const users = await addNewUser()
+    
+    const users = await addNewUser({
+      name: this.state.name,
+      avatarUrl: this.state.avatarUrl
+    })
   
-    this.setState({ users, userAdded: true })
+    // this.setState({ users, userAdded: true })
   }
 
   handleChange = name => event => {
@@ -39,20 +37,38 @@ class AddUser extends React.Component {
   render() {
     return (
       <Layout>
-        <form autoComplete="off">
+        <h1 style={styleH1}>Add new user</h1>
+        <Divider />
+        <form 
+          autoComplete="off"
+          style={styleForm}
+          onSubmit={this.onSubmit}
+        >
           <TextField
+            style={styleTextField}
             id="name"
-            label="name"
+            label="Name"
             value={this.state.name}
             onChange={this.handleChange('name')}
             margin="normal"
+            required
           />
-        </form>
-        <p>This users new</p>
-        <form onSubmit={this.onSubmit}>
-          <button variant="raised" type="submit">
-            Add
-          </button>
+          <TextField
+            style={styleTextField}
+            id="avatarUrl"
+            label="Avatar URL"
+            value={this.state.avatarUrl}
+            onChange={this.handleChange('avatarUrl')}
+            margin="normal"
+            required
+          />
+          <Button
+            variant="contained"
+            style={styleRaisedButton}
+            type="submit"
+          >
+            Add User
+          </Button>
         </form>
       </Layout>
     )
--- server/api/users.js
+++ server/api/users.js
@@ -15,61 +15,21 @@ router.get('/get', async (req, res) => {
   }
 })
 
-// router.post('/books/add', async (req, res) => {
-//   try {
-//     const book = await Book.add(Object.assign({ userId: req.user.id }, req.body));
-//     res.json(book);
-//   } catch (err) {
-//     logger.error(err);
-//     res.json({ error: err.message || err.toString() });
-//   }
-// })
-
 router.post('/add', (req, res) => {
+  const { name, avatarUrl } = req.body
+
   const userData = {
-        name: 'Mucci',
-        slug: 'tits',
-        points: 2,
-        avatarUrl: 'http://1.citynews-today.stgy.ovh/~media/original-hi/45879200115095/tacchino-2.jpg',
-        active: true
-      }
-  var newUser = new User(userData)
+    name,
+    slug: 'tits',
+    points: 1200,
+    avatarUrl,
+    active: true
+  }
+  const newUser = new User(userData)
   newUser.save(function (err) {
     if (err) return res.json({ error: err.message || err.toString() })
     res.json(newUser)
   })
-  // try {
-  //   const users = [
-  //     {
-  //       id: 1,
-  //       name: 'Mucci',
-  //       points: 1200
-  //     },
-  //     {
-  //       id: 2,
-  //       name: 'Moioli',
-  //       points: 1700
-  //     },
-  //     {
-  //       id: 3,
-  //       name: 'Slow',
-  //       points: 950
-  //     },
-  //     {
-  //       id: 4,
-  //       name: 'Boss',
-  //       points: 2500
-  //     },
-  //     {
-  //       id: 5,
-  //       name: 'Gabo',
-  //       points: 1200
-  //     }
-  //   ]
-  //   res.json(users);
-  // } catch (err) {
-  //   res.json({ error: err.message || err.toString() });
-  // }
 })
 
 module.exports = router
