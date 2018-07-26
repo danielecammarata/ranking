4439f038df526b05be904b407c4c6634f8aae587
Daniele Valentino Cammarata <DanieleValentino.Cammarata@vimn.com>
2018-07-26 11:51:03 +0200

    README update

--- pages/users/new.js
+++ pages/users/new.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+
 import Layout from '../../components/Layout.js'
 import { addNewUser, getUsersList } from '../../lib/api/users'
 
@@ -28,23 +30,30 @@ class AddUser extends React.Component {
     this.setState({ users, userAdded: true })
   }
 
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    })
+  }
+
   render() {
     return (
       <Layout>
+        <form autoComplete="off">
+          <TextField
+            id="name"
+            label="name"
+            value={this.state.name}
+            onChange={this.handleChange('name')}
+            margin="normal"
+          />
+        </form>
         <p>This users new</p>
         <form onSubmit={this.onSubmit}>
           <button variant="raised" type="submit">
             Add
           </button>
         </form>
-        <h2>Players</h2>
-        <ul>
-          {this.state.users.map(user => (
-            <li key={user.name}>
-              {user.name} | {user.points}
-            </li>
-          ))}
-        </ul>
       </Layout>
     )
   }
