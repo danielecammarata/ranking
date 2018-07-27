ee053d7c2df94670073a90872e0406678c73b74f
vergarag <Gianmarco.Vergara@vimnmix.com>
2018-07-27 13:43:24 +0200

    removed css file from live.js

--- pages/matches/live.js
+++ pages/matches/live.js
@@ -1,6 +1,5 @@
 import Layout from '../../components/Layout.js'
 import { styleLedBox, styleLedRed } from '../../lib/LedStyles.js'
-import "../../lib/animations.css"
 
 export default () => (
     <Layout>
