fa18079ea3472197f7834ccc113b9b674fc5155d
Daniele Valentino Cammarata <DanieleValentino.Cammarata@vimn.com>
2018-07-31 18:06:05 +0200

    turn off https redirect

--- server/index.js
+++ server/index.js
@@ -26,17 +26,17 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  if (!dev) {
-    server.use (function (req, res, next) {
-      if (req.secure) {
-        // request was via https, so do no special handling
-        next();
-      } else {
-        // request was via http, so redirect to https
-        res.redirect('https://scoreza.herokuapp.com' + req.url);
-      }
-    })
-  }
+  // if (!dev) {
+  //   server.use (function (req, res, next) {
+  //     if (req.secure) {
+  //       // request was via https, so do no special handling
+  //       next();
+  //     } else {
+  //       // request was via http, so redirect to https
+  //       res.redirect('https://scoreza.herokuapp.com' + req.url);
+  //     }
+  //   })
+  // }
 
   server.use(express.static('public'))
 
