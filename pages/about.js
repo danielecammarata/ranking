457dde0a125ecab111c65053e3c2c759c2634799
Evan Butera <Evan.Butera@vimnmix.com>
2018-07-31 15:14:41 +0200

    about content

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,119 @@
 import Layout from '../components/Layout.js'
 import MetaHead from '../components/MetaHead.js'
 
+const style = {
+  backgroundColor: 'rgba(255, 255, 255, 0.75)',
+  margin: '0px auto',
+  maxWidth: '480px',
+  padding: '30px 20px'
+}
+
 export default () => (
   <Layout>
-    {MetaHead('about')}
-    <p>This is the about page</p>
+    {MetaHead('Rules')}
+    <article style={style}>
+      <p>
+        <h2>கால்பந்து</h2>
+        kalpundiKumar è la divinità Indù del Calcetto
+      </p>
+      <p>
+        Ogni giocatore si impegna a rispettare le regole del gioco e a non violarle intenzionalmente. In questo senso
+        ciascun giocatore viene "elevato" al rango di arbitro, essendo egli stesso l'unico responsabile del suo
+        comportamento in campo, arrivando ad ammettere il proprio fallo.<br/>
+        Questo "voto" al rispetto delle regole e degli avversari è noto come Spirito Del Gioco (Spirit Of The Game ),
+        qualcosa di imprescindibile per poter giocare a calcetto.<br/>
+        La competizione estrema è incoraggiata a patto che questo non significhi infrangere le regole, perdere il
+        rispetto tra i giocatori o il genuino piacere di giocare.
+      </p>
+      <h3>Regolamento e Terminologia</h3>
+      <p>
+        Non ci sono arbitri, quindi le partite le arbitrano i giocatori, in caso di controversia e non si trova
+        l'accordo, verrà estratta a sorte la decisione tra le 2 squadre<br/>
+        <strong>esempio:</strong><br/>
+        Squadra 1 subisce un goal con presunto Gancio, Squadra 2 non ammette il Gancio.<br/>
+        Squadra 1 e Squadra 2 estrarranno a sorte la decisione
+      </p>
+      <p>
+        <h4>Regola del Gancio o Editto di Macrina</h4>
+        Si definisce gancio il doppio tocco da parte di omini sulla stessa stecca, questo può avvenire in due modi: il
+        primo omino "passa" la palla al secondo omino che tira oppure lo stesso omino ferma la palla e poi la tira.<br/>
+        Il doppio tocco tra omini di stecche diverse è consentito, ad esempio un omino del centrocampo passa la palla
+        all'omino dell'attacco che segna.<br/>
+        Un goal tramite gancio viene annullato.<br/>
+        Se, nonostante il gancio, non viene fatto goal il gioco continua senza interruzioni.<br/>
+        Per non incorrere nella regola del gancio, dopo il primo tocco, la palla deve rimbalzare su un bordo del campo,
+        su un omino avversario oppure su un proprio omino di un'altra stecca.<br/>
+        Se, a seguito di un gancio, la palla dovesse rimbalzare su un bordo del campo o su un omino avversario e poi
+        entrare in porta è da considerarsi comunque gancio.<br/>
+      </p>
+      <p>
+        <h4>Singola Sponda</h4>
+        quando si inserisce la pallina il gioco è valido solo dopo che abbia toccato la sponda opposta, o un omino, poi
+        il gioco sarà valido, di fatto non si può tirare al volo.<br/>
+        (In questo modo non si tira il volo!)
+      </p>
+      <p>
+        <h4>Palla Contesa</h4>
+        "palla contesa, palla alla difesa"<br/>
+        Quando la palla è bloccata e irraggiungibile tra le 2, nel caso sia tra attacco e difesa, la palla come da
+        tradizione verrà data alla difesa.
+      </p>
+      <p>
+        <h4>Rullata</h4>
+        Non si rulla, non siamo all'asilo!
+      </p>
+      <p>
+        <h4>Indian Scam</h4>
+        Tecnica tipica del subcontinente indiano, della zona di Chennay, che consiste nell'inserire la pallina per far
+        partire il gioco, con un effetto tale per cui la palla arrivi ai piedi dei propri attaccanti
+      </p>
+      <p>
+        <h4>Effetto M.Pinilla</h4>
+        Si dice Effetto M.Pinilla (dalla traversa di M.Pinilla durante il mondiale 2014
+        https://www.youtube.com/watch?v=ApW1y5fY_5M )<br/>
+        quando un giocatore prende un palo o una traversa nel momento del Goal Decisivo e poi la squadra avversaria
+        riesce a segnare il gol, con conseguente Rimonta
+      </p>
+      <p>
+        <h4>Sindrome di Harry Potter</h4>
+        Dalla Finale del Trofeo Harry Potter del Torneo della Millenaria.<br/>
+        La squadra che sul 5 a 0 non riesce a chiudere la partita e si fa rimontare, è una squadra con la Sindrome di
+        Harry Potter.
+      </p>
+      <p>
+        <h4>Eccesso di bullismo</h4>
+        L'eccesso di Bullismo avviene quando un giocatore subisce autogoal dopo aver tirato la pallina con troppa foga,
+        rimbalza sulla sponda avversaria e torna in gol
+      </p>
+      <p>
+        <h4>Manovra di Igor il Matto (o Igorata)</h4>
+        Igor il Matto consiste nel fare una serie di finte con i difensori, in modo da spiazzare l' attaccante
+        avversario.<br/>
+        La manovra di Igor il Matto venne fatta per la prima volta da Igor kazimir malevich nel Settembre del 1917
+        durante i primi moti che portarono alla Rivoluzione d'ottobre, si narra che fece 191 finte di fila senza mai
+        toccare il pallone.
+      </p>
+      <p>
+        <h4>Teorema di Bruno Vespa</h4>
+        Quando 2 portieri, solitamente bulli, intavolano un vivace botta e risposta.
+      </p>
+      <p>
+        <h4>La Maledetta di Gabo, Mossa del Bullo.</h4>
+        Passaggio tra difensore e portiere e conseguente goal del Portiere.
+      </p>
+      <p>
+        <h4>Scudo del Salumiere</h4>
+        Conosciuta anche come "colpo delle mille affettate di Mucci", tenta di creare uno scudo cinetico per
+        intercettare
+        le palle tra la metà campo avversaria e l'attacco e viceversa.<br/>
+        Purtroppo non sempre l'esito può essere alquanto imprevedibile.
+      </p>
+      <p>
+        <h4>Affettone o L'affettata del Salumiere</h4>
+        Manovra combinata della Zona Nord di Milano e Provincia, ma codificata nella zona rurale dell'Ucraina, la
+        manovra
+        consiste nell'evitare in maniera continua il tocco della pallina.
+      </p>
+    </article>
   </Layout>
 )
