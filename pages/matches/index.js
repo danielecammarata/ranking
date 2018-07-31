5e9ca31608d3b90c4ad8ab81ae6653af188bf521
Alessio Moioli <alessio.moioili@vimncontractor.com>
2018-07-31 14:43:51 +0200

    added load more on match list

--- pages/matches/index.js
+++ pages/matches/index.js
@@ -4,41 +4,79 @@ import { getMatchesList } from '../../lib/api/match'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
 import GridListTileBar from '@material-ui/core/GridListTileBar'
+import Button from '@material-ui/core/Button'
 
-function convertDate (inputFormat) {
-  function pad(s) { return (s < 10) ? '0' + s : s; }
-  var d = new Date(inputFormat);
-  return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/');
-}
+import { styleFormButton } from '../../lib/SharedStyles.js'
 
-const Index = (props) => (
-  <Layout>
-    <p>Played matches</p>
-    <GridList>
-      {props.matches.map(match => (
-        <GridListTile style={{height:'calc((50vw * 3 / 4) + 70px'}} key={match.slug}>
-          <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamHome.defender.avatarUrl} alt={match.teamHome.defender.name} />
-          <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamHome.striker.avatarUrl} alt={match.teamHome.striker.name} />
-          <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamAway.defender.avatarUrl} alt={match.teamAway.defender.name} />
-          <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamAway.striker.avatarUrl} alt={match.teamAway.striker.name} />
-          <img/>
-          <GridListTileBar
-            title={<span>{match.teamHome.defender.name}, {match.teamHome.striker.name} {match.teamHome.score} - {match.teamAway.score} {match.teamAway.defender.name}, {match.teamAway.striker.name}</span>}
-            subtitle={<span>{match.createdAt}</span>}
-          />
-        </GridListTile>
-      ))}
-    </GridList>
+class IndexMatch extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loadMoreActive: true,
+      matches: []
+    }
+    this.loadMore = this.loadMore.bind(this);
+  }
 
-  </Layout>
-)
+  async componentDidMount () {
+    try {
+      if(this.state.matches.length <= 0){
+        const data = await getMatchesList(0, 1)
+        this.setState({ matches: data })
+      }      
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-Index.getInitialProps = async function getInitialProps() {
-  const data = await getMatchesList()
+  loadMore () {
+    if(this.state.loadMoreActive === true) {
+      this.setState({ loadMoreActive: false })
+      try {
+        getMatchesList(this.state.matches.length, 1).then((newMatches) => {
+          const matches = this.state.matches.concat(newMatches)
+          this.setState({loadMoreActive: (matches.length != this.state.matches.length), matches: matches })
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 
-  return {
-    matches: data
+  convertDate (inputFormat) {
+    function pad(s) { return (s < 10) ? '0' + s : s; }
+    var d = new Date(inputFormat);
+    return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/');
+  }
+
+  render () {
+    return <Layout>
+      <p>Played matches</p>
+      <GridList>
+        {this.state.matches && this.state.matches.map(match => (
+          <GridListTile style={{height:'calc((50vw * 3 / 4) + 70px'}} key={match.slug}>
+            <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamHome.defender.avatarUrl} alt={match.teamHome.defender.name} />
+            <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamHome.striker.avatarUrl} alt={match.teamHome.striker.name} />
+            <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamAway.defender.avatarUrl} alt={match.teamAway.defender.name} />
+            <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamAway.striker.avatarUrl} alt={match.teamAway.striker.name} />
+            <img/>
+            <GridListTileBar
+              title={<span>{match.teamHome.defender.name}, {match.teamHome.striker.name} {match.teamHome.score} - {match.teamAway.score} {match.teamAway.defender.name}, {match.teamAway.striker.name}</span>}
+              subtitle={<span>{this.convertDate(match.createdAt)}</span>}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+      <Button
+        variant="contained"
+        style={styleFormButton}
+        type="button"
+        onClick={this.loadMore}
+        disabled={!this.state.loadMoreActive}
+      >
+        Load more
+      </Button>
+    </Layout>
   }
 }
-
-export default Index
+export default IndexMatch
