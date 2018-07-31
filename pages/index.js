39b811e94f65e250e7571b042a263173e3dfbcae
Evan Butera <Evan.Butera@vimnmix.com>
2018-07-31 17:21:16 +0200

    layout cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Grid from '@material-ui/core/Grid'
 import Layout from '../components/Layout.js'
 import MetaHead from '../components/MetaHead.js'
 
@@ -10,13 +8,6 @@ import {
 export default () => (
   <Layout>
     {MetaHead('home')}
-    <div style={{ padding: '10px 8%', fontSize: '15px' }}>
-      <Grid container direction="row" justify="space-around" align="flex-start">
-        <Grid item sm={12} xs={12} style={{ textAlign: 'center' }}>
-          <br />
-          <h1 style={styleH1}>Scoreza</h1>
-        </Grid>
-      </Grid>
-    </div>
+    <h1 style={styleH1}>Scoreza</h1>
   </Layout>
 )
--- pages/matches/index.js
+++ pages/matches/index.js
@@ -23,7 +23,7 @@ class IndexMatch extends React.Component {
       if(this.state.matches.length <= 0){
         const data = await getMatchesList(0, 1)
         this.setState({ matches: data })
-      }      
+      }
     } catch (err) {
       console.log(err)
     }
@@ -59,7 +59,6 @@ class IndexMatch extends React.Component {
             <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamHome.striker.avatarUrl} alt={match.teamHome.striker.name} />
             <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamAway.defender.avatarUrl} alt={match.teamAway.defender.name} />
             <img style={{float:'left', height:'calc(25vw * 3 / 4)', width: '50%'}} src={match.teamAway.striker.avatarUrl} alt={match.teamAway.striker.name} />
-            <img/>
             <GridListTileBar
               title={<span>{match.teamHome.defender.name}, {match.teamHome.striker.name} {match.teamHome.score} - {match.teamAway.score} {match.teamAway.defender.name}, {match.teamAway.striker.name}</span>}
               subtitle={<span>{this.convertDate(match.createdAt)}</span>}
