267fe9cb074e75741509b74eaa0c000ddcb59a8a
Daniele Valentino Cammarata <DanieleValentino.Cammarata@vimn.com>
2018-08-27 16:59:48 +0200

    match differences in home

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,216 @@
-import Grid from '@material-ui/core/Grid'
-import Layout from '../components/Layout'
 import withTitle from '../components/hoc/WithTitle'
 
+import Link from 'next/link'
+import Layout from '../components/Layout.js'
+import { getMatchesList } from '../lib/api/match'
 import {
-  styleH1,
-} from '../lib/SharedStyles.js'
+  styleLoadMoreButton,
+  styleMatchScore,
+  styleMatchDifference,
+  styleMatchTile,
+  stylePlayerScore,
+  styleTeamTile,
+  styleTeamPlayer
+} from '../lib/ListOfMatches.js'
+import { withStyles } from '@material-ui/core/styles'
 
-export default withTitle('Home | Scoreza')(() => (
-  <Layout>
-    <div style={{ padding: '10px 8%', fontSize: '15px' }}>
-      <Grid container direction="row" justify="space-around" align="flex-start">
-        <Grid item sm={12} xs={12} style={{ textAlign: 'center' }}>
-          <br />
-          <h1 style={styleH1}>Scoreza</h1>
-        </Grid>
-      </Grid>
-    </div>
-  </Layout>
-))
+import Avatar from '@material-ui/core/Avatar'
+import Badge from '@material-ui/core/Badge'
+import Button from '@material-ui/core/Button'
+import Chip from '@material-ui/core/Chip'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import { Divider, Typography } from '@material-ui/core'
+import green from '@material-ui/core/colors/green'
+import red from '@material-ui/core/colors/red'
+
+const elementPerPage = 6
+
+const styles = {
+  chipsLabel: {
+    display: 'inline-block',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  winBadge: {
+    backgroundColor: green[500]
+  },
+  loseBadge: {
+    colorPrimary: red[500],
+    backgroundColor: red[500]
+  }
+}
+class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loadMoreActive: true,
+      matches: [],
+      matchesObj: {}
+    }
+    this.loadMore = this.loadMore.bind(this);
+  }
+
+  async componentDidMount () {
+    try {
+      if(this.state.matches.length <= 0){
+        const data = await getMatchesList(0, elementPerPage)
+
+        const matchesObj = this.prepareMatchData(data)
+
+        this.setState({ matches: data, matchesObj: matchesObj })
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  loadMore () {
+    if(this.state.loadMoreActive === true) {
+      this.setState({ loadMoreActive: false })
+      try {
+        getMatchesList(this.state.matches.length, elementPerPage).then((newMatches) => {
+          const matches = this.state.matches.concat(newMatches)
+          const matchesObj =this.prepareMatchData(newMatches)
+          this.setState({loadMoreActive: (matches.length > this.state.matches.length + elementPerPage), matches: matches, matchesObj: matchesObj })
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
+  /**
+   * Group match data by createdAt date
+   * @param {array} data - matches array
+   * @returns {object} grouped matches
+   */
+  prepareMatchData (data) {
+    const matches = this.state.matchesObj
+    data.forEach(item => {
+      const currDate = this.convertDate(item.createdAt)
+      if (currDate in matches) {
+        matches[currDate].matches.push(item)
+      } else {
+        matches[currDate] = {
+          matches: [item]
+        }
+      }
+    })
+
+    return matches
+  }
+
+  convertDate (inputFormat) {
+    function pad(s) { return (s < 10) ? '0' + s : s; }
+    var d = new Date(inputFormat)
+    return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/')
+  }
+
+  differenceTile = (hasWin, classes, difference, styleMatchDifference) => (
+    <Badge
+      color={hasWin ? 'primary' : 'secondary'}
+      classes={{
+        colorPrimary: classes.winBadge,
+        colorSecondary: classes.loseBadge
+      }}
+      badgeContent={<small>{hasWin ? difference : - difference}</small>}
+      style={styleMatchDifference}
+    />
+  )
+
+  matchTile = (label, matches) => (
+    <GridListTile style={styleMatchTile} key={label}>
+      <Typography>
+        {label}
+      </Typography>
+      <Divider />
+      <GridList style={{margin: '0 auto', maxWidth: '500px', minWidth: '400px'}}>
+        {matches.map(match => (
+          <GridListTile style={styleMatchTile} key={match.slug}>
+            <GridList>
+              <GridListTile style={styleTeamTile('right')}>
+                <Chip
+                  avatar={<Avatar src={match.teamHome.defender.avatarUrl} />}
+                  label={match.teamHome.defender.name}
+                  style={styleTeamPlayer('right')}
+                />
+                <Chip
+                  avatar={<Avatar src={match.teamHome.striker.avatarUrl} />}
+                  label={match.teamHome.striker.name}
+                  style={styleTeamPlayer('right')}
+                />
+                {
+                  this.differenceTile(
+                    (match.teamHome.score > match.teamAway.score),
+                    this.props.classes,
+                    match.difference,
+                    styleMatchDifference
+                  )
+                }
+              </GridListTile>
+              <GridListTile style={styleMatchScore}>
+                <Link as={`/match/${match._id}`} href={`/matches/detail/?slug=${match._id}`}>
+                  <Button variant="fab" mini color="primary">
+                    {match.teamHome.score} - {match.teamAway.score}
+                  </Button>
+                </Link>
+                <Badge color="secondary" badgeContent={<small>{match.teamHome.defScore}</small>} style={stylePlayerScore('defender','home')}> </Badge>
+                <Badge color="secondary" badgeContent={<small>{match.teamHome.strScore}</small>} style={stylePlayerScore('striker','home')}> </Badge>
+                <Badge color="secondary" badgeContent={<small>{match.teamAway.defScore}</small>} style={stylePlayerScore('defender','away')}> </Badge>
+                <Badge color="secondary" badgeContent={<small>{match.teamAway.strScore}</small>} style={stylePlayerScore('striker','away')}> </Badge>
+              </GridListTile>
+              <GridListTile style={styleTeamTile('left')}>
+                <Chip
+                  avatar={<Avatar src={match.teamAway.defender.avatarUrl} />}
+                  classes={{label: this.props.classes.chipsLabel}}
+                  label={match.teamAway.defender.name}
+                  style={styleTeamPlayer('left')}
+                />
+                <Chip
+                  avatar={<Avatar src={match.teamAway.striker.avatarUrl} />}
+                  label={match.teamAway.striker.name}
+                  style={styleTeamPlayer('left')}
+                />
+                {
+                  this.differenceTile(
+                    (match.teamAway.score > match.teamHome.score),
+                    this.props.classes,
+                    match.difference,
+                    styleMatchDifference
+                  )
+                }
+              </GridListTile>
+            </GridList>
+          </GridListTile>
+        ))}
+      </GridList>
+    </GridListTile>
+  )
+
+  render () {
+    const { matchesObj } = this.state
+    return (
+      <Layout>
+        {/* New grouped matches list */}
+        <GridList style={{margin: '0 auto', maxWidth: '500px', minWidth: '400px'}}>
+          {Object.keys(matchesObj).map(matchKey => (
+            this.matchTile(matchKey,  matchesObj[matchKey].matches)
+          ))}
+        </GridList>
+        <Button
+          color="primary"
+          disabled={!this.state.loadMoreActive}
+          onClick={this.loadMore}
+          style={styleLoadMoreButton(this.state.loadMoreActive)}
+          type="button"
+          variant="contained"
+        >
+          Load more
+        </Button>
+      </Layout>
+    )
+  }
+}
+
+export default withTitle('Home | Scoreza')(withStyles(styles)(Index))
