dfb0283bb28f05cdbce0f9f65da37ed9b6383d5d
Daniele Valentino Cammarata <DanieleValentino.Cammarata@vimn.com>
2018-08-29 18:12:49 +0200

    ranking list styles

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,13 +1,44 @@
 import Link from 'next/link'
 import Divider from '@material-ui/core/Divider'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 
 import Layout from '../../components/Layout.js'
 import { getUsersList, deleteUser } from '../../lib/api/users'
 import { styleH1, styleCard, styleCardContainer, styleCardContent, styleBigAvatar } from '../../lib/SharedStyles'
+
+import Avatar from '@material-ui/core/Avatar'
+
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+
+import {
+  AlienIcon,
+  BombIcon,
+  BoneIcon,
+  CandycaneIcon,
+  CannabisIcon,
+  CatIcon,
+  ChessKingIcon,
+  DogIcon,
+  DuckIcon,
+  EmoticonPoopIcon,
+  FoodIcon,
+  GhostIcon,
+  NinjaIcon,
+  NukeIcon,
+  PigIcon,
+  PirateIcon,
+  RocketIcon,
+  SkullIcon,
+  SoccerFieldIcon,
+  SoccerIcon,
+  TrophyIcon,
+  WeatherSnowyIcon
+} from '../../components/IconComponents'
+import lightBlue from '@material-ui/core/colors/lightBlue'
 
 class IndexUser extends React.Component {
   constructor(props) {
@@ -42,37 +73,57 @@ class IndexUser extends React.Component {
       <Layout>
         <h1 style={styleH1}>Ranking</h1>
         <Divider />
-        <ul
-          style={{
-            listStyle: 'none'
-          }}
-        >
-          {this.state.users && this.state.users.map(user => (
-            <li
+        <List>
+          {this.state.users && this.state.users.map((user, index) => (
+            <Link as={`/users/${user.slug}`} href={`/users/update/?slug=${user.slug}`}>
+            <ListItem
               key={user.slug}
-              style={{
-                marginBottom: '10px'
-              }}
+              divider
             >
-              <Link as={`/users/${user.slug}`} href={`/users/update/?slug=${user.slug}`}>
-                <Card style={styleCard}>
-                  <div style={styleCardContainer}>
-                    <CardContent style={styleCardContent}>
-                      <Typography variant="headline">{user.name}</Typography>
-                      <Typography variant="subheading" color="textSecondary">Points: {user.points}</Typography>
-                    </CardContent>
-                  </div>
-                  <CardMedia
-                    style={styleBigAvatar}
-                    image={user.avatarUrl}
-                    title={user.name}
+
+                <Typography
+                  style={{
+                    paddingRight: 15,
+                    fontSize: 22
+                  }}
+                >
+                  {index + 1}
+                </Typography>
+                <ListItemAvatar>
+                  <Avatar
+                    alt={user.name}
+                    src={user.avatarUrl}
+                    style={{
+                      height: 80,
+                      width: 80
+                    }}
                   />
-                </Card>
-              {/* <button variant="raised" onClick={(e) => this.removeUser(user, e)}>X</button> */}
+                </ListItemAvatar>
+                <ListItemText
+                  primary={user.name}
+                  primaryTypographyProps={{
+                    style: {
+                      fontSize: 15
+                    }
+                  }}
+                  secondary={user.points}
+                  secondaryTypographyProps={{
+                    style: {
+                      fontSize: 18,
+                      color: lightBlue[500]
+                    }
+                  }}
+                />
+                {/*
+                  THIS SHOULD BE USED FOR THE BADGES
+                <ListItemSecondaryAction>
+                  <PirateIcon />
+                  <RocketIcon />
+                </ListItemSecondaryAction> */}
+            </ListItem>
               </Link>
-            </li>
           ))}
-        </ul>
+        </List>
       </Layout>
     )
   }
