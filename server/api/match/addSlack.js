6e4a76f574d235565cf09f2641faefba4d270586
Alessio Moioli <alessio.moioili@vimncontractor.com>
2018-09-14 13:02:58 +0200

    proto slack match insert

--- server/api/match/addSlack.js
+++ server/api/match/addSlack.js
@@ -3,9 +3,6 @@ const express = require('express')
 const router = express.Router()
 
 const { createEventAdapter } = require('@slack/events-api')
-
-// const slackEvents = createEventAdapter('9867b34f13115ea3839ddc91713b73a2')
-console.log(process.env)
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET)
 
 const Match = require('../../models/Match')
@@ -14,39 +11,122 @@ const slack = require('../../lib/slack')
 
 const rankify = require('../../lib/rankify')
 
-router.post('/', async (req, res) => {
+router.get('/test', async (req, res) => {
+  const rs = await User.find({ active: true })
+  console.log('All users!!!')
+  console.log(rs)
+})
+
+router.get('/', async (req, res) => {
   slackEvents.expressMiddleware()
+  var value = req.body.event.text
+  // var value = '<@UCPLWJ6E5> <@UCS26NM6C> - <@UCQHH9YRK> <@UCSDP1Z4H> : 6-0'
   const newMatchPattern = /^(<@[A-Z0-9]{9,}>) (<@[A-Z0-9]{9,}>) - (<@[A-Z0-9]{9,}>) (<@[A-Z0-9]{9,}>) : (0?[1-9][0-9]|[0-9])-(0?[1-9][0-9]|[0-9])/gm
-  const command = req.body.event.text.replace(process.env.SLACK_BOT_ID + ' ', '')
+  const command = value.replace(process.env.SLACK_BOT_ID + ' ', '')
   const condition = command.match(newMatchPattern)
   console.log('COMMAND => ', command)
   if ( condition !== null) {
     const [ teams, scores ] = command.split(' : ')
     const [ homeTeam, awayTeam ] = teams.split(' - ')
     const [ homeDef, homeStr ] = homeTeam.split(' ')
-    const [ awayDef, awayStr ] = homeTeam.split(' ')
+    const [ awayDef, awayStr ] = awayTeam.split(' ')
     const [ homeScore, awayScore ] = scores.split('-')
+    Promise.all([
+      await findPlayerPromise(homeDef, "Home Defender"),
+      await findPlayerPromise(homeStr, "Home Striker"),
+      await findPlayerPromise(awayDef, "Away Defender"),
+      await findPlayerPromise(awayStr, "Away Striker")
+    ]).then((rs) => {
+      const scoreAndBadges = calculateScoreAndBadges(homeScore, awayScore)
+      const createdAt = new Date().toISOString()
+      const slug = Match.generateSlug()
+      const rank = rankify.calculate({
+        teamHome: {
+          defender: rs[0],
+          striker: rs[1],
+          score: homeScore,
+        },
+        teamAway: {
+          defender: rs[2],
+          striker: rs[3],
+          score: awayScore,
+        }
+      })      
+      const matchData = {
+        teamHome: {
+          defBadges: [],
+          strBadges: [],
+          defender: rs[0],
+          striker: rs[1],
+          score: homeScore,
+          defScore: scoreAndBadges.homeScoreDefender,
+          strScore: scoreAndBadges.homeScoreStriker
+        },
+        teamAway: {
+          defBadges: [],
+          strBadges: [],
+          defender: rs[2],
+          striker: rs[3],
+          score: awayScore,
+          defScore: scoreAndBadges.awayScoreDefender,
+          strScore: scoreAndBadges.awayScoreStriker
+        },
+        badges: scoreAndBadges.badges,
+        slug,
+        createdAt,
+        difference: rank.difference
+      }
+      newMatch.save(function (err) {
+        if (err) return res.json({ error: err.message || err.toString() })
+
+        const scoreHD = {
+          id: teamHome.defender._id,
+          score: rank.homeDefense,
+          stats: calculateStats(teamHome.defender, true, rank.hasHomeWin, teamHome, teamAway, rank.difference, rank.homeDefense),
+          res
+        }
+        updateUser(scoreHD)
+
+        const scoreHS = {
+          id: teamHome.striker._id,
+          score: rank.homeStriker,
+          stats: calculateStats(teamHome.striker, false, rank.hasHomeWin, teamHome, teamAway, rank.difference, rank.homeStriker),
+          res
+        }
+        updateUser(scoreHS)
+
+        const scoreAD = {
+          id: teamAway.defender._id,
+          score: rank.awayDefense,
+          stats: calculateStats(teamAway.defender, true, !rank.hasHomeWin, teamAway, teamHome, rank.difference, rank.awayDefense),
+          res
+        }
+        updateUser(scoreAD)
+
+        const scoreAS = {
+          id: teamAway.striker._id,
+          score: rank.awayStriker,
+          stats: calculateStats(teamAway.striker, false, !rank.hasHomeWin, teamAway, teamHome, rank.difference, rank.awayStriker),
+          res
+        }
+        updateUser(scoreAS)
+        slack.sendMessage(
+          `
+          --- Match Successfully Inserted ---
+          ${teamHome.defender.name} + ${teamHome.striker.name} : ${teamHome.score}
+          vs
+          ${teamAway.defender.name} + ${teamAway.striker.name} : ${teamAway.score}
+          `, 
+          process.env.SLACK_TOKEN,
+          process.env.SLACK_CHANNEL_ID
+        )
+        res.json(newMatch)
+      })    
+    }).catch((e) => {
+      console.log(e)
+    })
     
-    const hdPlayer = findPlayerPromise(homeDef, "Home Defender")
-    const hsPlayer = findPlayerPromise(homeStr, "Home Striker")
-    const adPlayer = findPlayerPromise(awayDef, "Away Defender")
-    const asPlayer = findPlayerPromise(awayStr, "Away Striker")
-    console.log('--------------')
-    console.log(hdPlayer)
-    console.log(hsPlayer)
-    console.log(adPlayer)
-    console.log(asPlayer)
-    console.log('--------------')
-    slack.sendMessage(
-      `
-      --- Match data ---
-      ${homeTeam} : ${homeScore}
-      vs
-      ${awayTeam} : ${awayScore}
-      `, 
-      process.env.SLACK_TOKEN,
-      process.env.SLACK_CHANNEL_ID
-    )
+
 
     // tasks.reduce(function(cur, next) {
     //     return cur.then(next);
@@ -82,73 +162,104 @@ router.post('/', async (req, res) => {
   res.json({ "challenge": req.body.challenge })
 })
 
-slackEvents.on('message', (event) => {
-  console.log('Events: ')
-  console.log(event)
-  slack.sendMessage(
-    '1Command Key Found!!!', 
-    process.env.SLACK_TOKEN,
-    process.env.SLACK_CHANNEL_ID
-  )
+// slackEvents.on('message', (event) => {
+//   console.log('Events: ')
+//   console.log(event)
+//   slack.sendMessage(
+//     '1Command Key Found!!!', 
+//     process.env.SLACK_TOKEN,
+//     process.env.SLACK_CHANNEL_ID
+//   )
   
   
-  // const { teamAway, teamHome, badges } = req.body
+//   // const { teamAway, teamHome, badges } = req.body
 
-  // const slug = Match.generateSlug()
-  // const createdAt = new Date().toISOString()
+//   // const slug = Match.generateSlug()
+//   // const createdAt = new Date().toISOString()
 
-  // const rank = rankify.calculate({
-  //   teamHome,
-  //   teamAway
-  // })
-  // const matchData = {
-  //   teamHome,
-  //   teamAway,
-  //   badges,
-  //   slug,
-  //   createdAt,
-  //   difference: rank.difference
+//   // const rank = rankify.calculate({
+//   //   teamHome,
+//   //   teamAway
+//   // })
+//   // const matchData = {
+//   //   teamHome,
+//   //   teamAway,
+//   //   badges,
+//   //   slug,
+//   //   createdAt,
+//   //   difference: rank.difference
+//   // }
+
+//   // const newMatch = new Match(matchData)
+//   // newMatch.save(function (err) {
+//   //   if (err) return res.json({ error: err.message || err.toString() })
+
+//   //   const scoreHD = {
+//   //     id: teamHome.defender._id,
+//   //     score: rank.homeDefense,
+//   //     stats: calculateStats(teamHome.defender, true, rank.hasHomeWin, teamHome, teamAway, rank.difference, rank.homeDefense),
+//   //     res
+//   //   }
+//   //   updateUser(scoreHD)
+
+//   //   const scoreHS = {
+//   //     id: teamHome.striker._id,
+//   //     score: rank.homeStriker,
+//   //     stats: calculateStats(teamHome.striker, false, rank.hasHomeWin, teamHome, teamAway, rank.difference, rank.homeStriker),
+//   //     res
+//   //   }
+//   //   updateUser(scoreHS)
+
+//   //   const scoreAD = {
+//   //     id: teamAway.defender._id,
+//   //     score: rank.awayDefense,
+//   //     stats: calculateStats(teamAway.defender, true, !rank.hasHomeWin, teamAway, teamHome, rank.difference, rank.awayDefense),
+//   //     res
+//   //   }
+//   //   updateUser(scoreAD)
+
+//   //   const scoreAS = {
+//   //     id: teamAway.striker._id,
+//   //     score: rank.awayStriker,
+//   //     stats: calculateStats(teamAway.striker, false, !rank.hasHomeWin, teamAway, teamHome, rank.difference, rank.awayStriker),
+//   //     res
+//   //   }
+//   //   updateUser(scoreAS)
+
+//   //   res.json(newMatch)
+//   // })
+// })
+
+const calculateScoreAndBadges = (homeScore, awayScore) => {
+  const scoreAndBadges = {
+    badges: []
+  }
+  if (homeScore === 0 || awayScore === 0) {
+    scoreAndBadges.badges.push('cappotto')
+  }
+  scoreAndBadges.homeScoreDefender = Math.floor(homeScore / 2)
+  scoreAndBadges.homeScoreStriker = Math.ceil(homeScore / 2)
+  // if (this.state.homeScoreDefender + this.state.homeScoreStriker !== this.state.homeScore) {
+  //   scoreAndBadges.homeScoreDefender = Math.floor(this.state.homeScore / 2)
+  //   scoreAndBadges.homeScoreStriker = Math.ceil(this.state.homeScore / 2)
+  // }
+  // else {
+  //   scoreAndBadges.homeScoreDefender = this.state.homeScoreDefender
+  //   scoreAndBadges.homeScoreStriker = this.state.homeScoreStriker
+  // }
+  scoreAndBadges.awayScoreDefender = Math.floor(awayScore / 2)
+  scoreAndBadges.awayScoreStriker = Math.ceil(awayScore / 2)
+  // if (this.state.awayScoreDefender + this.state.awayScoreStriker !== this.state.awayScore) {
+  //   scoreAndBadges.awayScoreDefender = Math.floor(this.state.awayScore / 2)
+  //   scoreAndBadges.awayScoreStriker = Math.ceil(this.state.awayScore / 2)
+  // }
+  // else {
+  //   scoreAndBadges.awayScoreDefender = this.state.awayScoreDefender
+  //   scoreAndBadges.awayScoreStriker = this.state.awayScoreStriker
   // }
 
-  // const newMatch = new Match(matchData)
-  // newMatch.save(function (err) {
-  //   if (err) return res.json({ error: err.message || err.toString() })
-
-  //   const scoreHD = {
-  //     id: teamHome.defender._id,
-  //     score: rank.homeDefense,
-  //     stats: calculateStats(teamHome.defender, true, rank.hasHomeWin, teamHome, teamAway, rank.difference, rank.homeDefense),
-  //     res
-  //   }
-  //   updateUser(scoreHD)
-
-  //   const scoreHS = {
-  //     id: teamHome.striker._id,
-  //     score: rank.homeStriker,
-  //     stats: calculateStats(teamHome.striker, false, rank.hasHomeWin, teamHome, teamAway, rank.difference, rank.homeStriker),
-  //     res
-  //   }
-  //   updateUser(scoreHS)
-
-  //   const scoreAD = {
-  //     id: teamAway.defender._id,
-  //     score: rank.awayDefense,
-  //     stats: calculateStats(teamAway.defender, true, !rank.hasHomeWin, teamAway, teamHome, rank.difference, rank.awayDefense),
-  //     res
-  //   }
-  //   updateUser(scoreAD)
-
-  //   const scoreAS = {
-  //     id: teamAway.striker._id,
-  //     score: rank.awayStriker,
-  //     stats: calculateStats(teamAway.striker, false, !rank.hasHomeWin, teamAway, teamHome, rank.difference, rank.awayStriker),
-  //     res
-  //   }
-  //   updateUser(scoreAS)
-
-  //   res.json(newMatch)
-  // })
-})
+  return scoreAndBadges
+}
 
 const calculateStats = (user, isDefender, winner, team, oppositeTeam, rankDifference, newPoints) => {
   const currentStats = user.stats
@@ -181,24 +292,12 @@ const calculateStats = (user, isDefender, winner, team, oppositeTeam, rankDiffer
   }
 }
 
-const findPlayerPromise = (homeDef, position) => {
-  return findUserBySlackID(homeDef, position).then((rs) => {
-    if (!rs) return res.json({ error: `Unable to find ${position} player.` })
-    return rs
-  })
-}
-
-const findUserBySlackID = ({slackID}) => {
-  const query = { slackID: slackID }
-  User.find(query, {}, function (err, rs) {
-    if (err) {
-      console.error(err)
-      return false
-    }
-    console.log('Found player:')
-    console.log(rs)
-    return rs
-  })
+const findPlayerPromise = async (homeDef, position) => {
+  const rs = await User.find({ slackID: homeDef })
+  if (!rs || rs.length <= 0) return res.json({ error: `Unable to find ${position} player.` })
+  console.log(position)
+  console.log(rs)
+  return rs
 }
 
 const updateUser = ({id, score, stats, res}) => {
