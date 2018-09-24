6b99bd17e19b34e0b55e39f83aebe4be23907df9
Alessio Moioli <alessio.moioili@vimncontractor.com>
2018-09-24 12:08:41 +0200

    removed redirect from code

--- server/index.js
+++ server/index.js
@@ -40,17 +40,17 @@ function isAuthenticated(req, res, next) {
 app.prepare().then(() => {
   const server = express()
 
-  if (!dev) {
-    server.use (function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] == 'https') {
-        // request was via https, so do no special handling
-        next();
-      } else {
-        // request was via http, so redirect to https
-        res.redirect('https://' + req.headers.host + req.url);
-      }
-    })
-  }
+  // if (!dev) {
+  //   server.use (function (req, res, next) {
+  //     if (req.headers['x-forwarded-proto'] == 'https') {
+  //       // request was via https, so do no special handling
+  //       next();
+  //     } else {
+  //       // request was via http, so redirect to https
+  //       res.redirect('https://' + req.headers.host + req.url);
+  //     }
+  //   })
+  // }
 
   server.use(express.static('public'))
 
